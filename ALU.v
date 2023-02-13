60e374f30471474eb2da22aff4fae1610238216c
Huzaifa Javed <48155033+huzaifaj530@users.noreply.github.com>
2023-02-13 16:26:36 +0500

    lab 3 imp

---
+++ ALU.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+module ALU(a,b,s,out);
+input [7:0] a,b;
+input [3:0] s;
+output [7:0] out;
+reg [7:0] out;
+
+always@(*)
+begin
+	case(s)
+	3'b000:
+		out=a+b;
+	3'b001:
+		out=a-b;	
+	3'b010:
+		out=a*b;
+	3'b011:
+		out=a/b;
+	3'b100:
+		out=a&b;
+	3'b101:
+		out=a|b;
+	3'b110:
+		out=a^b;
+	3'b111:
+		out=~(a^b);
+	endcase 
+end
+endmodule
---
+++ aluB_TestBench.v
@@ -0,0 +1,60 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   16:17:02 02/13/2023
+// Design Name:   ALU
+// Module Name:   C:/Users/DSA Lab/Desktop/2021234/lab3/aluB_TestBench.v
+// Project Name:  lab3
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: ALU
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module aluB_TestBench;
+
+	// Inputs
+	reg [7:0] a;
+	reg [7:0] b;
+	reg [3:0] s;
+
+	// Outputs
+	wire [7:0] out;
+
+	// Instantiate the Unit Under Test (UUT)
+	ALU uut (
+		.a(a), 
+		.b(b), 
+		.s(s), 
+		.out(out)
+	);
+integer i;
+	initial begin
+		// Initialize Inputs
+		a = 3;b = 2;s = 0;#10
+		
+		for (i=0;i<=7;i=i+1)
+      begin
+       s = s + 1;
+       #10;
+      end;
+
+		// Wait 100 ns for global reset to finish
+$finish;
+		// Add stimulus here
+
+	end
+      
+endmodule
+
