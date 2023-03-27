e4794eff0535c3013a704c6f8c5b6408afa149a8
Huzaifa Javed <48155033+huzaifaj530@users.noreply.github.com>
2023-03-27 13:28:33 +0500

    Add files via upload

---
+++ lab7/Mealy_NonOverlapping_FSM_TB.v
@@ -0,0 +1,75 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   13:18:10 03/27/2023
+// Design Name:   Mealy_nonOverlapping_FSM
+// Module Name:   C:/Users/DSA Lab/Desktop/2021234/lab7_Mealy_Machine_FSM/Mealy_NonOverlapping_FSM_TB.v
+// Project Name:  lab7_Mealy_Machine_FSM
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: Mealy_nonOverlapping_FSM
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module Mealy_NonOverlapping_FSM_TB;
+
+	// Inputs
+	reg clock;
+	reg reset;
+	reg x;
+
+	// Outputs
+	wire y;
+
+	// Instantiate the Unit Under Test (UUT)
+	Mealy_nonOverlapping_FSM uut (
+		.clock(clock), 
+		.reset(reset), 
+		.x(x), 
+		.y(y)
+	);
+
+integer i;
+reg [19:0] data;
+
+always#5clock=~clock;
+	initial begin
+		// Initialize Inputs
+		clock = 0;
+		reset = 0;
+		x = 0;
+			
+		data=20'b11101011011011011111;
+		
+		reset=1;#10;
+		reset=0;#10;
+		
+		for(i=0;i<20;i=i+1)
+		begin
+			x=data[i];
+			#10;
+		end
+
+		$finish;
+        
+		// Add stimulus here
+		// Wait 100 ns for global reset to finish
+
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
---
+++ lab7/Mealy_nonOverlapping_FSM.v
@@ -0,0 +1,75 @@
+`timescale 1ns / 1ps
+
+module Mealy_nonOverlapping_FSM(clock,reset,x,y);
+
+input clock; // clock signal
+input reset; // reset input
+input x; // binary input
+output reg y;
+
+parameter  s0=3'b000; // "Zero" State
+parameter  s1=3'b001;
+parameter  s2=3'b010;
+parameter  s3=3'b011;
+parameter  s4=3'b100;
+
+
+
+reg [2:0] current_state, next_state;
+always @(current_state or x)
+begin
+	case(current_state)
+	s0:
+	begin
+	if(x==1)
+	next_state=s1;
+	else
+	next_state=current_state;
+	end
+	
+	s1:
+	begin
+	if(x==1)
+	next_state=s2;
+	else
+	next_state=s0;
+	end
+	
+	s2:begin
+	if(x==0)
+	next_state=s3;
+	else
+	next_state=s2;
+	end
+	
+	s3:begin
+	if(x==1)
+	next_state=s4;
+	else
+	next_state=s0;
+	end
+	
+	s4:begin
+	if(x==1)
+	next_state=s0;
+	else
+	next_state=s2;
+	end
+	
+	default:next_state=s0;
+	endcase
+end
+
+
+always@(posedge clock)
+begin 
+	y=0;
+	if(current_state==s4 && x==1)
+		y=1;	
+	if(reset)
+	current_state=s0;
+	else
+	current_state=next_state;
+end
+
+endmodule
