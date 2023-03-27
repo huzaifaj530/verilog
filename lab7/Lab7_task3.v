c4839dabd49d7b133c6c092365733de3e5ce1963
Huzaifa Javed <48155033+huzaifaj530@users.noreply.github.com>
2023-03-27 13:59:08 +0500

    Add files via upload

---
+++ lab7/Lab7_task3.v
@@ -0,0 +1,86 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:29:52 03/27/2023 
+// Design Name: 
+// Module Name:    Lab7_task3 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Lab7_task3(clock,reset,x,y);
+
+input clock; // clock signal
+input reset; // reset input
+input x; // binary input
+output reg y;
+
+parameter  s0=2'b00; // "Zero" State
+parameter  s1=2'b01;
+parameter  s2=2'b10;
+parameter  s3=2'b11;
+
+
+reg [1:0] current_state, next_state;
+always @(current_state or x)
+begin
+	case(current_state)
+	s0:
+	begin
+	if(x==0)
+	next_state=s1;
+	else
+	next_state=current_state;
+	end
+	
+	s1:
+	begin
+	if(x==1)
+	next_state=s2;
+	else
+	next_state=current_state;
+	end
+	
+	s2:begin
+	if(x==0)
+	next_state=s3;
+	else
+	next_state=s0;
+	end
+	
+	
+	s3:begin
+	if(x==0)
+	next_state=s0;
+	else
+	next_state=s2;
+	end
+	
+	default:next_state=s0;
+	endcase
+end
+
+
+always@(posedge clock)
+begin 
+	y=0;
+	if(current_state==s3&&x==0)
+	y=1;
+	if(reset)
+	current_state=s0;
+	else
+	current_state=next_state;
+end
+
+
+endmodule
---
+++ lab7/lab7_task3_TB.v
@@ -0,0 +1,75 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   13:49:07 03/27/2023
+// Design Name:   Lab7_task3
+// Module Name:   C:/Users/DSA Lab/Desktop/2021234/lab7_Mealy_Machine_FSM/lab7_task3_TB.v
+// Project Name:  lab7_Mealy_Machine_FSM
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: Lab7_task3
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module lab7_task3_TB;
+
+	// Inputs
+	reg clock;
+	reg reset;
+	reg x;
+
+	// Outputs
+	wire y;
+
+	// Instantiate the Unit Under Test (UUT)
+	Lab7_task3 uut (
+		.clock(clock), 
+		.reset(reset), 
+		.x(x), 
+		.y(y)
+	);
+
+integer i;
+reg [27:0] data;
+
+always#5clock=~clock;
+	initial begin
+		// Initialize Inputs
+		clock = 0;
+		reset = 0;
+		x = 0;
+			
+		data=28'b0010000000100001001000110100;
+		
+		reset=1;#10;
+		reset=0;#10;
+		
+		for(i=0;i<28;i=i+1)
+		begin
+			x=data[i];
+			#10;
+		end
+
+		$finish;
+        
+		// Add stimulus here
+		// Wait 100 ns for global reset to finish
+
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
