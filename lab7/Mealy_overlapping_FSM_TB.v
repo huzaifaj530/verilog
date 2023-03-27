eedb496347fbea6aa7854405c87ddc913d85af84
Huzaifa Javed <48155033+huzaifaj530@users.noreply.github.com>
2023-03-27 12:54:06 +0500

    lab7

---
+++ lab7/Mealy_overlapping_FSM_TB.v
@@ -0,0 +1,67 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   12:40:53 03/27/2023
+// Design Name:   mealy_overlapping_FSM
+// Module Name:   C:/Users/DSA Lab/Desktop/2021234/lab7_Mealy_Machine_FSM/Mealy_overlapping_FSM_TB.v
+// Project Name:  lab7_Mealy_Machine_FSM
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: mealy_overlapping_FSM
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module Mealy_overlapping_FSM_TB;
+
+	// Inputs
+	reg clock;
+	reg reset;
+	reg x;
+
+	// Outputs
+	wire y;
+
+	// Instantiate the Unit Under Test (UUT)
+	mealy_overlapping_FSM uut (
+		.clock(clock), 
+		.reset(reset), 
+		.x(x), 
+		.y(y)
+	);
+always#5clock=~clock;
+	initial begin
+		// Initialize Inputs
+		clock = 0;
+		reset = 0;
+		x = 0;
+			
+		#10;x=1;
+		#10;x=0;
+		#10;x=0;
+		#10;x=1;
+		#10;x=0;
+		#10;x=1;
+		#10;x=0;
+		#10;x=1;
+		#10;
+
+		// Wait 100 ns for global reset to finish
+		$finish;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
---
+++ lab7/mealy_overlapping_FSM.v
@@ -0,0 +1,77 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:31:43 03/27/2023 
+// Design Name: 
+// Module Name:    mealy_overlapping_FSM 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module mealy_overlapping_FSM(clock,reset,x,y);
+
+input clock; // clock signal
+input reset; // reset input
+input x; // binary input
+output reg y;
+
+parameter  s0=2'b00; // "Zero" State
+parameter  s1=2'b01;
+parameter  s2=2'b10;
+
+
+reg [1:0] current_state, next_state;
+always @(current_state or x)
+begin
+	y=0;
+	case(current_state)
+	s0:
+	begin
+	if(x==1)
+	next_state=s1;
+	else
+	next_state=current_state;
+	end
+	
+	s1:
+	begin
+	if(x==0)
+	next_state=s2;
+	else
+	next_state=current_state;
+	end
+	
+	s2:begin
+	if(x==1)begin
+	y=1;
+	next_state=s1;
+	end
+	else
+	next_state=s0;
+	end
+	
+	default:next_state=s0;
+	endcase
+end
+
+
+always@(posedge clock)
+begin 
+	y=0;
+	if(reset)
+	current_state=s0;
+	else
+	current_state=next_state;
+end
+
+endmodule
