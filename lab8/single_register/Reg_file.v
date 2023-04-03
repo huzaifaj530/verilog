f65a429368a40ca5bd5f4b329146d2194c46e218
Huzaifa Javed <48155033+huzaifaj530@users.noreply.github.com>
2023-04-03 13:02:47 +0500

    Add files via upload

---
+++ lab8/single_register/Reg_file.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+
+
+module Reg_file(
+
+input clk,wr,
+input [3:0] r1,rw,
+input [7:0] data_in,
+output [7:0] out1  );
+
+reg [7:0] Registers [3:0];
+
+reg [7:0] out1;
+
+
+always@(posedge clk)begin
+	if(wr)
+		Registers[rw]<=data_in;
+	else
+		begin
+		out1<=Registers[r1];
+		end
+end
+endmodule
